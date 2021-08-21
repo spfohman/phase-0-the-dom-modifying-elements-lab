582d1991cdd0d92b960f64e764dc414ee42f263b
Sarah <spfohman@email.essex.edu>
2021-08-21 07:56:28 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+var el = document.getElementById('main');
+el.remove();
+
+var newHeader = document.createElement("h1");
+newHeader.id='victory';
+newHeader.innerHTML="YOUR-NAME is the champion";
